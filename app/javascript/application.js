d09d32ac189576d7f109d9918606091b5573e099
Marcel Schmidt <marcel.schmidt@sapiens.com>
2023-06-18 20:13:55 +0200

    Added index styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require rails-ujs
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
+import "bootstrap/dist/js/bootstrap"
+
