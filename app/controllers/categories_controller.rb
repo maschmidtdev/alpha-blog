0e7e5a722e92df5ca9dce74fa40fbedfb00e3c76
Marcel Schmidt <marcel.schmidt@sapiens.com>
2023-06-21 11:58:59 +0200

    Create category and test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,15 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    end
   end
   def index
 
@@ -9,6 +17,11 @@ class CategoriesController < ApplicationController
 
   def show
 
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
